4a243d40516264d0b63f37e95babc87b47808701
Marcin Rabiak <marcin.rabiak@devexpress.com>
2025-09-18 13:08:48 +0200

    initial commit

---
+++ CS/Data/McpResources.cs
@@ -0,0 +1,20 @@
+﻿using ModelContextProtocol.Protocol;
+using ModelContextProtocol.Server;
+
+namespace AIChatMcpResources.Data;
+
+[McpServerResourceType]
+public class McpResource {
+    static string GetFilePath(string fileName) => Path.Combine(AppContext.BaseDirectory, "Data", fileName);
+    static string ReadTextResource(string fileName) => File.ReadAllText(GetFilePath(fileName));
+    static string ReadBlobResource(string fileName) => Convert.ToBase64String(File.ReadAllBytes(GetFilePath(fileName)));
+
+    [McpServerResource(Name = "Access log", MimeType = "text/plain")]
+    public static string Log() => ReadTextResource("access.txt");
+
+    [McpServerResource(Name = "AIChat documentation", MimeType = "text/markdown")]
+    public static string Docs() => ReadTextResource("dxaichat.md");
+
+    [McpServerResource(Name = "Dashboard screenshot", MimeType = "image/jpeg")]
+    public static BlobResourceContents Dashboard() => new() { Blob = ReadBlobResource("dashboard.jpg") };
+}
---
+++ CS/EnvSettings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace AIChatMcpResources;
+
+public static class EnvSettings {
+    public static string AzureOpenAIEndpoint => Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+    public static string AzureOpenAIKey => Environment.GetEnvironmentVariable("AZURE_OPENAI_APIKEY");
+    public static string DeploymentName => "GPT4o";
+}
